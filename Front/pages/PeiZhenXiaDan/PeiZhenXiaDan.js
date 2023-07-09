29948350ea0570b5d3a8711bffb38f17f2a9a6f1
LetMeFly666 <814114971@qq.com>
2023-07-09 21:45:17 +0800

    实现了陪诊下单页面（借用了主页的样式代码）

--- Front/pages/PeiZhenXiaDan/PeiZhenXiaDan.js
+++ Front/pages/PeiZhenXiaDan/PeiZhenXiaDan.js
@@ -1,66 +1,132 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2023-07-03 21:15:24
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2023-07-09 21:43:33
+ */
 // pages/PeiZhenXiaDan/PeiZhenXiaDan.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        serviceType: [{
+            id: 1,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '特需门诊VIP陪诊',
+            description: '尊贵特许门诊VIP，车接车送，不错的选择',
+            price: '588'
+        }, {
+            id: 2,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '军、警、消及家属陪诊',
+            description: '军、警、消及家属陪诊，不错的选择',
+            price: '688'
+        }, {
+            id: 3,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '全心陪诊（半天/全天）',
+            description: '全心陪诊，也是一个不错的选择',
+            price: '788'
+        }, {
+            id: 4,
+            img: '/images/img/serviceType2_01.jpg',
+            title: 'VIP陪诊（半天/全天）',
+            description: 'VIP陪诊，也是一个不错的选择',
+            price: '888'
+        }, {
+            id: 5,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '陪诊超时服务',
+            description: '咱不怕超时，也是一个不错的选择',
+            price: '988'
+        }, {
+            id: 6,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '门诊约号',
+            description: '门诊约号，也是一个不错的选择',
+            price: '88'
+        }, {
+            id: 7,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '专家约诊',
+            description: '专家约诊，还是一个不错的选择',
+            price: '188'
+        }, {
+            id: 8,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '代办问诊',
+            description: '代办问诊，同样是一个不错的选择',
+            price: '288'
+        }, {
+            id: 9,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '代取结果',
+            description: '代取结果，也还是一个不错的选择',
+            price: '388'
+        }, {
+            id: 10,
+            img: '/images/img/serviceType2_01.jpg',
+            title: '代办买药',
+            description: '代办买药，最后还是一个不错的选择',
+            price: '488'
+        }]
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    }
 })
