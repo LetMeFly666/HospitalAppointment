381fc450104270b7c08822fc7a3aee5ff9d44cc9
LetMeFly666 <814114971@qq.com>
2023-08-17 18:02:13 +0800

    django.core.exceptions.DisallowedHost: Invalid HTTP_HOST header: '127.0.0.1:8005'. You may need to add '127.0.0.1' to ALLOWED_HOSTS

--- back/HospitalAppointment/settings.py
+++ back/HospitalAppointment/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = Secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
