1c25dc7aff75178dc6a92b9687e3ac387c59debd
LetMeFly666 <814114971@qq.com>
2023-08-17 18:09:22 +0800

    暂时关掉debug

--- back/HospitalAppointment/settings.py
+++ back/HospitalAppointment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = Secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
