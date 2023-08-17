c71ccb89415d1b1ab4cbd4550f2b3fcdb5ee2d25
LetMeFly666 <814114971@qq.com>
2023-08-17 17:56:12 +0800

    CommandError: You must set settings.ALLOWED_HOSTS if DEBUG is False.

--- back/HospitalAppointment/settings.py
+++ back/HospitalAppointment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = Secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
