de2c64ab4b2ecf214fc09c10f740be24e55960d4
LetMeFly666 <814114971@qq.com>
2023-08-17 16:37:24 +0800

    test

--- back/HospitalAppointment/settings.py
+++ back/HospitalAppointment/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'HospitalAppointment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': Secrets.DATABASE_ADDRESS,
+        'PORT': '3306',
+        'USER': Secrets.DATABASE_USER,
+        'PASSWORD': Secrets.DATABASE_PASSWORD,
+        'NAME': Secrets.DATABASE_NAME,
     }
 }
 
