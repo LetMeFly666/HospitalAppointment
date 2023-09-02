d5ad3d1d3d5a0d4d8e4016d7c7adb95b16c9b2a1
LetMeFly666 <814114971@qq.com>
2023-09-02 10:43:13 +0800

    完成了医院的医生列表

--- Front/pages/ExpertsList/ExpertsList.js
+++ Front/pages/ExpertsList/ExpertsList.js
@@ -1,66 +1,99 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2023-09-01 22:46:41
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2023-09-02 10:22:27
+ */
 // pages/ExpertsList/ExpertsList.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        id: '',
+        hospitalInfo: {},
+        expertList: []  // 本医院的医生列表
+    },
 
-  },
+    genExpertList: function(allExperts, expertIds) {
+        const ans = [];
+        for (var i = 0; i < allExperts.length; i++) {
+            if (expertIds.indexOf(String(allExperts[i].id)) != -1) {
+                ans.push(allExperts[i]);
+            }
+        }
+        return ans;
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        var hospitalInfo = {};
+        for (var i = 0; i < app.hospitalList.length; i++) {
+            if (String(options.id) == String(app.hospitalList[i].id)) {
+                hospitalInfo = app.hospitalList[i];
+                break;
+            }
+        }
 
-  },
+        var expertList = this.genExpertList(app.expertList, hospitalInfo.experts);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+        this.setData({
+            id: options.id,
+            hospitalInfo: hospitalInfo,
+            expertList: expertList
+        });
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+    },
 
-  }
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
