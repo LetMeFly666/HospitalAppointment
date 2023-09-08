05c89da9d20f51b4d2e5f9a5dc7f27a96d26b6a7
LetMeFly666 <814114971@qq.com>
2023-09-08 16:06:14 +0800

    陪诊经验（陪诊案例）

--- Front/pages/PeiZhen_Example/PeiZhen_Example.js
+++ Front/pages/PeiZhen_Example/PeiZhen_Example.js
@@ -1,66 +1,85 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2023-09-02 12:20:49
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2023-09-08 15:51:49
+ */
 // pages/PeiZhenExample/PeiZhenExample.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        id: '',
+        hospitalInfo: {},
+        exampleInfo: ['2023年1月12日', '2023年3月10日', '2023年3月31日', '2023年5月1日', '2023年6月2日']
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        var hospitalInfo = {};
+        for (var i = 0; i < app.hospitalList.length; i++) {
+            if (String(options.id) == String(app.hospitalList[i].id)) {
+                hospitalInfo = app.hospitalList[i];
+                break;
+            }
+        }
+        this.setData({
+            id: options.id,
+            hospitalInfo: hospitalInfo
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    }
 })
