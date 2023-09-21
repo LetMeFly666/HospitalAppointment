1e36019c8ce41a219beb1fbc1699eb3a9ad1a482
LetMeFly666 <814114971@qq.com>
2023-09-21 22:08:11 +0800

    try pic

--- back/HospitalAppointment/settings.py
+++ back/HospitalAppointment/settings.py
@@ -123,11 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+if not DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
