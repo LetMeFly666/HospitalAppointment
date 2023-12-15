bc25fecdf0150a40026972bf1ed443a2d55480d4
LetMeFly666 <814114971@qq.com>
2023-12-15 17:03:29 +0800

    添加就诊人v1 and half

--- Front/pages/FriendAdd/FriendAdd.js
+++ Front/pages/FriendAdd/FriendAdd.js
@@ -1,66 +1,102 @@
 // pages/FriendAdd/FriendAdd.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        if18: '',
+        name: '',
+        sex: '',
+        phone: '',
+        relations: ['父母', '子女', '兄弟姐妹', '本人', '夫妻', '其他'],
+        relation: '',
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    // set1newValue(valueName) {},
 
-  },
+    yesorno(event) {
+        const val = event.detail.value;
+        this.setData({
+            if18: val
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    inputname(event) {
+        const val = event.detail.value;
+        this.setData({
+            name: val
+        });
+    },
 
-  },
+    manorwoman(event) {
+        const val = event.detail.value;
+        this.setData({
+            sex: val
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    inputphone(event) {
+        const val = event.detail.value;
+        this.setData({
+            phone: val
+        });
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+    },
 
-  }
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
