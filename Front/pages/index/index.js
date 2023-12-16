60e66063b46db6e3fec6fceda71357db5bc44751
LetMeFly666 <814114971@qq.com>
2023-12-16 16:17:06 +0800

    首页的“点击服务跳转”

--- Front/pages/index/index.js
+++ Front/pages/index/index.js
@@ -1,9 +1,21 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2023-07-03 18:08:50
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2023-12-16 16:15:44
+ */
 // index.js
 const app = getApp();
 Page({
     data: {
         hospitalList: []
     },
+
+    change2PeiZhenXiaDanPage() {
+        console.log('clicked')
+        wx.switchTab({'url': '/pages/PeiZhenXiaDan/PeiZhenXiaDan'});
+    },
+
     onLoad: function(options) {
         this.setData({
             hospitalList: app.hospitalList
