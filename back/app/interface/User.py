d3837af799b939a2605c98ca61d5e148232787bb
LetMeFly666 <814114971@qq.com>
2023-12-18 23:08:25 +0800

    try serializers.serialize

--- back/app/interface/User.py
+++ back/app/interface/User.py
@@ -2,10 +2,11 @@
 Author: LetMeFly
 Date: 2023-09-20 16:16:47
 LastEditors: LetMeFly
-LastEditTime: 2023-12-18 23:06:18
+LastEditTime: 2023-12-18 23:08:06
 Description: 人员相关（用户信息、 就诊人、陪诊员）
 '''
 from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 from app import models
 from app.baseFunction import randmod
 import Secrets
@@ -73,6 +74,6 @@ def getFriends(request):
     warrant = request.GET.get('warrant')
     userid = models.User.objects.get(warrant=warrant).userid
     friends = models.Friend.objects.filter(friend=userid)
-    friends = friends.values()
+    friends = serializers.serialize('json', friends)
     print(friends)
     return JsonResponse({'msg': '查询成功！', 'data': friends})
